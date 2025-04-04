e9957d84b45baba2324b3de441c7d796b17d7dc1
yassgan <yassinegana.gana@gmail.com>
2025-04-04 12:56:48 +0100

    Fresh start

---
+++ yolo_inference.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+model =YOLO('yolov8x')
+
+results=model.predict('input_videos/input_vid.mp4',save=True) ##It will output the same video with the boxes of the detected objects
+print(results[0]) ##The result of the first frame
+
+
+print("---------------------------")
+for box in results[0].boxes: ##The box referes to the detected object
+    print(box)
